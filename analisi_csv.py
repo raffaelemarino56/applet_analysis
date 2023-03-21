d855777ae71a8e82b4a539249d52eec7f3356746
raffaelemarino56 <raffaelemarino56@gmail.com>
2023-03-21 20:18:49 +0100

    aggiunta fig2

--- analisi_csv.py
+++ analisi_csv.py
@@ -3,7 +3,7 @@ import plotly.express as px
 
 #https://plotly.com/python/plotly-express/#gallery
 
-with open('./dati.csv') as csv_file:
+with open('../dati.csv') as csv_file:
 
     df=pd.read_csv(csv_file)
 
@@ -15,20 +15,21 @@ with open('./dati.csv') as csv_file:
     aggiunte_per_giorno = aggiunte_per_giorno.sort_values("created", ascending=True)
     fig1 = px.line(aggiunte_per_giorno, x="created", y=["addCount"], title="Andamento aggiunte al giorno")
     fig1.show()
-    
-    #grafico a barre per creatorName
-    aggiunte_da_craetore = df.groupby("creatorName")["addCount"].sum().reset_index()
-    aggiunte_da_craetore = aggiunte_da_craetore.sort_values("creatorName", ascending=False)
-    fig2 = px.bar(aggiunte_da_craetore, x="creatorName", y=["addCount"], title="Num di azioni create per creatore")
-    fig2.show()
     """
+
+    #grafico a barre per creatorName
+    aggiunte_da_craetore = df.groupby("creatorName").sum().reset_index()
+    aggiunte_da_craetore = aggiunte_da_craetore.sort_values("creatorName", ascending=False)
+    fig2 = px.bar(aggiunte_da_craetore, x="creatorName", y="addCount", title="Num di azioni create per creatore")
+    #fig2.show()
+    fig2.write_image("../fig2.jpeg")
     
     #grafico a torta che raggruppa per creatorName
     preferenze_per_creatore = df.groupby("creatorName").sum().reset_index()
     preferenze_per_creatore = preferenze_per_creatore.sort_values("creatorName", ascending=False)
-    fig3 = px.pie(preferenze_per_creatore, values="creatorName", names="creatorName", title="Distribuzione per creatore")
-    fig3.show()
-    fig3.write_image("./fig3.jpeg")
+    fig3 = px.pie(preferenze_per_creatore, values="creatorName", names="addCount", title="Distribuzione per creatore")
+    #fig3.show()
+    fig3.write_image("../fig3.jpeg")
 
     
     
